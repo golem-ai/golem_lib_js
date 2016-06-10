25ac220e4d99ce73419c8fdf5b1abdaaa1d0c9df
Killian Vermersch <killian.vermersch@gmail.com>
2016-06-10 12:39:43 +0200

    New general config behaviour

--- golem.js
+++ golem.js
@@ -13,22 +13,98 @@ function log(message) {
     console.log(message);
 }
 
-
 /*
 ** Framework
 */
 var lang_fr = "fr-fr";
 var lang_en = "en-us";
 
+var golem_log_no_config = false;
+
+function golem_no_config(client, data)
+{
+    if (golem_log_no_config)
+    {
+	log(client.name + " no config for " + data.type + ":");
+	console.log(data);
+    }
+}
+
+var config = {
+    //common
+    "handshake": golem_no_config, 
+    "confirm_identity": golem_no_config,
+    "error": golem_no_config,
+    "confirm_request": golem_no_config,
+    // Front
+    "answer": golem_no_config,
+    "set_fixed_time_ok": golem_no_config,
+    "request": golem_no_config,
+    "say": golem_no_config,
+    "call": golem_no_config,
+    // Target
+    "confirm_interaction": golem_no_config,
+    "confirm_interaction_array": golem_no_config,
+    "confirm_delete_interaction": golem_no_config,
+    "confirm_enable_interaction": golem_no_config,
+    "confirm_disable_interaction": golem_no_config,
+    "confirm_enable_interaction_array": golem_no_config,
+    "confirm_disable_interaction_array": golem_no_config,
+    "confirm_say_to_all": golem_no_config,
+    "confirm_say": golem_no_config,
+    "confirm_enable_all_but": golem_no_config,
+    "confirm_disable_all_but": golem_no_config
+};
+
+var golem_log_no_config_core = false;
+
+function golem_no_config_core(client, evt)
+{
+    if (golem_log_no_config_core)
+    {
+	log(client.name + " : no config_core for :");
+	console.log(evt);
+    }
+}
+
+var config_core = {
+    "on_open":golem_no_config_core,
+    "on_error":golem_no_config_core,
+    "on_message":golem_no_config_core,
+    "on_close":golem_no_config_core,
+    "on_send":golem_no_config_core,
+    "on_request":golem_no_config_core
+};
+
+function merge_config(base_config, user_config)
+{
+    var config_final = {};
+
+    for (var attr in base_config)
+    {
+	config_final[attr] = base_config[attr];
+    }
+
+    if (typeof user_config == 'object' && user_config != null)
+    {
+	for (var attr in user_config)
+	{
+	    config_final[attr] = user_config[attr];
+	}
+    }
+
+    return config_final;
+}
+
 class GolemCore {
     /*
     ** Public
     */
-    constructor(host, port, onOpenFct, onErrorFct, onMsgFct, onCloseFct, onSendFct) {
+    constructor(host, port, config_core_user, config_user) { //onOpenFct, onErrorFct, onMsgFct, onCloseFct, onSendFct) {
+	this.config_core = merge_config(config_core, config_core_user);
+	this.config = merge_config(config, config_user);
 	this.identity = 'unidentified';
-	this.name = '';
-        this.call_map = {};
-	this.add_parsing_function('send', onSendFct);
+	this.name = '*newborn*';
         
         this.connected = false;
         var host = "ws://" + host + ":" + port;
@@ -37,22 +113,26 @@ class GolemCore {
         
         this.socket.onopen = function(evt) {
             client.connected = true;
+	    var onOpenFct = client.config_core["on_open"];
 	    if (typeof onOpenFct == 'function')
 		onOpenFct(client, evt);
         };
         this.socket.onerror = function(evt) {
             client.connected = false;
+	    var onErrorFct = client.config_core["on_error"];
 	    if (typeof onErrorFct == 'function')
 		onErrorFct(client, evt);
         };
         this.socket.onmessage = function(evt) {
             client.last_packet_received = evt;
+	    var onMsgFct = client.config_core["on_message"];
 	    if (typeof onMsgFct == 'function')
 		onMsgFct(client, evt);
             client.parsing(evt.data);
         };
         this.socket.onclose = function(evt) {
             client.connected = false;
+	    var onCloseFct = client.config_core["on_close"];
 	    if (typeof onCloseFct == 'function')
 		onCloseFct(client, evt);
         };
@@ -62,7 +142,7 @@ class GolemCore {
         message = JSON.stringify(message);
         this.socket.send(message);
         this.last_packet_sent = message;
-        var call = this.call_map['send'];
+        var call = this.config_core["on_send"];
         if (typeof call == 'function')
 	    call(this, message);
     }
@@ -76,6 +156,7 @@ class GolemCore {
     */
     
     identify(identity, name, id_session) {
+    	this.name = name;
         this.send({
     	    type:"identity",
     	    category:identity,
@@ -83,23 +164,22 @@ class GolemCore {
     	    name: name
     	});
     	this.identity = identity + "_not_confirmed";
-    	this.name = name;
     }
     
-    add_parsing_function(key, call) {
-        if (typeof call != 'function' && call != null) {
-            console.log("warning ! Paramer function call -", key, " is unset");
-            return;
-        }
-        this.call_map[key] = call;
-    }
+    // add_parsing_function(key, call) {
+    //     if (typeof call != 'function' && call != null) {
+    //         console.log("warning ! Parameter function call -", key, " is unset");
+    //         return;
+    //     }
+    //     this.call_map[key] = call;
+    // }
     
     parsing(msg) {
         var obj = JSON.parse(msg);
         var key = obj.type;
 	if (key == "confirm_identity")
             this.identity = obj.category;
-        var call = this.call_map[key];
+        var call = this.config[key];
         if (typeof call != 'function' && call != null) {
             console.log("Received unknown action ", key, " in ", this.identity);
             console.log("Msg->", msg);
@@ -112,14 +192,14 @@ class GolemCore {
 
 
 class GolemFront extends GolemCore {
-    setParsingFct(identityConfirm, requestConfirm, answer, setFixedTimeOk, onRequest, onError) {
-        this.add_parsing_function("confirm_identity", identityConfirm);
-        this.add_parsing_function("confirm_request", requestConfirm);
-        this.add_parsing_function("answer", answer);
-        this.add_parsing_function("set_fixed_time_ok", setFixedTimeOk);
-        this.add_parsing_function("request", onRequest);
-        this.add_parsing_function("error", onError);
-    }
+    // setParsingFct(identityConfirm, requestConfirm, answer, setFixedTimeOk, onRequest, onError) {
+    //     this.add_parsing_function("confirm_identity", identityConfirm);
+    //     this.add_parsing_function("confirm_request", requestConfirm);
+    //     this.add_parsing_function("answer", answer);
+    //     this.add_parsing_function("set_fixed_time_ok", setFixedTimeOk);
+    //     this.add_parsing_function("request", onRequest);
+    //     this.add_parsing_function("error", onError);
+    // }
     
     identify(name, id_session) {
         super.identify("front", name, id_session);
@@ -131,8 +211,8 @@ class GolemFront extends GolemCore {
     	    language:lang,
     	    text:text
     	};
-        this.send(request);
-        var call = this.call_map['request'];
+	this.send(request);
+        var call = this.config_core['on_request'];
         if (typeof call == 'function')
 	    call(this, request);
     }
@@ -151,13 +231,13 @@ class GolemFront extends GolemCore {
 }
 
 class GolemTarget extends GolemCore {
-    setParsingFct(identityConfirm, call, confirm_interaction, confirm_interaction_array, onError) {
-        this.add_parsing_function("confirm_identity", identityConfirm);
-        this.add_parsing_function("call", call);
-        this.add_parsing_function("confirm_interaction", confirm_interaction);
-        this.add_parsing_function("confirm_interaction_array", confirm_interaction_array);
-        this.add_parsing_function("error", onError);
-    }
+    // setParsingFct(identityConfirm, call, confirm_interaction, confirm_interaction_array, onError) {
+    //     this.add_parsing_function("confirm_identity", identityConfirm);
+    //     this.add_parsing_function("call", call);
+    //     this.add_parsing_function("confirm_interaction", confirm_interaction);
+    //     this.add_parsing_function("confirm_interaction_array", confirm_interaction_array);
+    //     this.add_parsing_function("error", onError);
+    // }
     
     identify(name, id_session) {
         super.identify("target", name, id_session);
@@ -204,18 +284,18 @@ class GolemFrontAndTarget extends GolemCore {
         super.identify("front_and_target", name, id_session);
     }
     
-    setParsingFct(identityConfirm, onError) {
-        this.add_parsing_function("confirm_identity", identityConfirm);
-        this.add_parsing_function("error", onError);
-    }
+    // setParsingFct(identityConfirm, onError) {
+    //     this.add_parsing_function("confirm_identity", identityConfirm);
+    //     this.add_parsing_function("error", onError);
+    // }
     
     //Front c/c
-    setParsingFctFront(requestConfirm, answer, setFixedTimeOk, onRequest) {
-        this.add_parsing_function("confirm_request", requestConfirm);
-        this.add_parsing_function("answer", answer);
-        this.add_parsing_function("set_fixed_time_ok", setFixedTimeOk);
-        this.add_parsing_function("request", onRequest);
-    }
+    // setParsingFctFront(requestConfirm, answer, setFixedTimeOk, onRequest) {
+    //     this.add_parsing_function("confirm_request", requestConfirm);
+    //     this.add_parsing_function("answer", answer);
+    //     this.add_parsing_function("set_fixed_time_ok", setFixedTimeOk);
+    //     this.add_parsing_function("request", onRequest);
+    // }
     
     sendRequest(lang, text) {
 	    var request = {
@@ -242,11 +322,11 @@ class GolemFrontAndTarget extends GolemCore {
     }
     
     // Target c/c
-    setParsingFctTarget(call, confirm_interaction, confirm_interaction_array) {
-        this.add_parsing_function("call", call);
-        this.add_parsing_function("confirm_interaction", confirm_interaction);
-        this.add_parsing_function("confirm_interaction_array", confirm_interaction_array);
-    }
+    // setParsingFctTarget(call, confirm_interaction, confirm_interaction_array) {
+    //     this.add_parsing_function("call", call);
+    //     this.add_parsing_function("confirm_interaction", confirm_interaction);
+    //     this.add_parsing_function("confirm_interaction_array", confirm_interaction_array);
+    // }
     
     interactionArray(array) {
         this.send({
